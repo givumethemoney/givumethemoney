0b43c7bbfd9f3ce2017d18bc735993f01a288dff
zzadu <zzadu08@naver.com>
2024-10-14 00:29:37 +0900

    feat: repository 정리

--- src/main/java/com/hey/givumethemoney/repository/DonationRepository.java
+++ src/main/java/com/hey/givumethemoney/repository/DonationRepository.java
@@ -1,6 +1,22 @@
 package com.hey.givumethemoney.repository;
 
 import com.hey.givumethemoney.domain.Donation;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DonationRepository extends JPADonationRepository<Donation>{
+import java.util.List;
+import java.util.Optional;
+
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+    @Override
+    <S extends Donation> S save(S entity);
+
+    @Override
+    Optional<Donation> findById(Long aLong);
+
+    @Override
+    void deleteById(Long aLong);
+
+    @Override
+    List<Donation> findAll(Sort sort);
 }
--- src/main/java/com/hey/givumethemoney/repository/WaitingDonationRepository.java
+++ src/main/java/com/hey/givumethemoney/repository/WaitingDonationRepository.java
@@ -1,7 +1,23 @@
 package com.hey.givumethemoney.repository;
 
-import com.hey.givumethemoney.domain.DonationBase;
 import com.hey.givumethemoney.domain.WaitingDonation;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WaitingDonationRepository extends JPADonationRepository<WaitingDonation> {
+import java.util.List;
+import java.util.Optional;
+
+public interface WaitingDonationRepository extends JpaRepository<WaitingDonation, Long> {
+
+    @Override
+    <S extends WaitingDonation> S save(S entity);
+
+    @Override
+    Optional<WaitingDonation> findById(Long aLong);
+
+    @Override
+    void deleteById(Long aLong);
+
+    @Override
+    List<WaitingDonation> findAll(Sort sort);
 }
