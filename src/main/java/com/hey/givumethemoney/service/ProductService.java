f0ba3eb6c98cb1a80dc3dcefb456c7222ce1ee2d
zzadu <zzadu08@naver.com>
2024-10-14 14:33:58 +0900

    feat: 기부 품목 테이블 추가

---
+++ src/main/java/com/hey/givumethemoney/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.hey.givumethemoney.service;
+
+import com.hey.givumethemoney.domain.Product;
+import com.hey.givumethemoney.repository.ProductRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void saveProducts(List<Product> productList) {
+        for (Product p : productList) {
+            productRepository.save(p);
+        }
+    }
+
+    public List<Product> getProductsByDonationId(Long id) {
+        List<Product> productList = productRepository.findAll();
+
+        List<Product> result = new ArrayList<>();
+        for (Product p : productList) {
+            if (p.getDonationId().equals(id)) {
+                result.add(p);
+            }
+        }
+
+        return result;
+    }
+}
