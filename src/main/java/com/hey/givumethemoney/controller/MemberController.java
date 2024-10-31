1d1f14cc883fbc4e3ff7e043dc47e27999233c21
hibar-02 <20211121@sungshin.ac.kr>
2024-10-31 17:11:36 +0900

    feat : 회원가입, 로그인 구현

--- src/main/java/com/hey/givumethemoney/controller/MemberController.java
+++ src/main/java/com/hey/givumethemoney/controller/MemberController.java
@@ -63,16 +63,9 @@ public class MemberController {
 
         // 사용자 존재 여부와 비밀번호 확인
         if (memberDomain != null && passwordEncoder.matches(password, memberDomain.getPassword())) {
-            sessreion.setAttribute("loggedInUser", memberDomain);
-
-            //사용자 역할에 따라 redirect
-            Role role = memberDomain.getRole();
-                if ("ADMIN".equals(role)) {
-                    return "redirect:/admin";
-                } else if ("COMPANY".equals(role)) {
-                    return "redirect:/company";
-                } else return "redirect:/";
-            }
+            session.setAttribute("loggedInUser", memberDomain);
+            return "redirect:/";
+        }
 
         // 로그인 실패 시, 에러 메시지를 표시하거나 로그인 페이지로 리다이렉트
         redirectAttributes.addFlashAttribute("error", "이메일 혹은 비밀번호를 확인하세요."); // 에러 메시지 추가
