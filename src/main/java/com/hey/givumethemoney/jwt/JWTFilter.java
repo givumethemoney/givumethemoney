f603378001e35074cf2349c497b2345446bd4211
zzadu <zzadu08@naver.com>
2024-12-03 21:26:11 +0900

    refactor: 불필요한 부분 제거

--- src/main/java/com/hey/givumethemoney/jwt/JWTFilter.java
+++ src/main/java/com/hey/givumethemoney/jwt/JWTFilter.java
@@ -40,23 +40,6 @@ public class JWTFilter extends OncePerRequestFilter {
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-            // 로그인, 회원가입 등의 URL은 JWT 필터를 실행하지 않도록 설정
-            if (request.getRequestURI().equals("/") ||
-                request.getRequestURI().startsWith("/login") ||
-                request.getRequestURI().startsWith("/join") ||
-                request.getRequestURI().startsWith("/css") ||
-                request.getRequestURI().startsWith("/js") ||
-                request.getRequestURI().startsWith("/images") ||
-                request.getRequestURI().startsWith("/image") ||
-                request.getRequestURI().startsWith("/favicon.ico") ||
-                request.getRequestURI().startsWith("/payments") ||
-                request.getRequestURI().startsWith("/receipts") ||
-                request.getRequestURI().startsWith("/receiptList")) {
-                System.out.println(request.getRequestURI());
-                filterChain.doFilter(request, response); // JWT 필터를 건너뛰고 다음 필터로 이동
-                return;
-            }
-
         System.out.println("JWTFilter is processing request: " + request.getRequestURI());
 
         // 쿠키에서 JWT 토큰을 찾음
