adae13183c10c7c08524c39049b94e69445b7db7
joiejuni <lumiere.298@gmail.com>
2024-12-09 09:23:01 +0900

    feat: intro 페이지 작성

--- src/main/java/com/hey/givumethemoney/config/SecurityConfig.java
+++ src/main/java/com/hey/givumethemoney/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hey.givumethemoney.config;
+package com.hey.givumethemoney.Config;
 
 import com.hey.givumethemoney.jwt.JWTFilter;
 import com.hey.givumethemoney.jwt.JWTUtil;
@@ -45,7 +45,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // 특정 URL은 인증 없이 접근 가능(permitAll())
                         .requestMatchers("/login", "/join", "/", "/css/**", "/js/**", "/images/**", "/image/*", "/error", "/webfonts/*", "/chatbot/**", "/json/**").permitAll()
-                        .requestMatchers("/detail/*", "/payments", "/pay", "/success","/receipts/**", "/receiptList/*","/donationList", "/donationList/**").permitAll()
+                        .requestMatchers("/detail/*", "/payments", "/pay", "/success","/receipts/**", "/receiptList/*","/donationList", "/donationList/**", "/donationIntro").permitAll()
                         .requestMatchers(HttpMethod.POST, "/login-fail").permitAll()
                         // 특정 역할이 있어야만 접근 가능한 URL(hasRole("ROLE"))
                         .requestMatchers("/applicationList/*", "/application/agree", "/application/write", "/application/edit",
--- src/main/java/com/hey/givumethemoney/controller/InquiryController.java
+++ src/main/java/com/hey/givumethemoney/controller/InquiryController.java
@@ -2,6 +2,7 @@ package com.hey.givumethemoney.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,6 +36,11 @@ public class InquiryController {
         System.out.println(inquiry);
 
         return "redirect:" + redirectUrl;
+    }
+
+    @GetMapping("/donationIntro")
+    public String donationIntro() {
+        return "donationIntro";
     }
 
 }
